9bd0e59120b1556794888dff0e676bb05a0bee2c
Mayorbest <mayowaayeni69@gmail.com>
2026-01-05 12:07:08 +0100

    Add files via upload

--- BrealLoginP.js
+++ BrealLoginP.js
@@ -1,3 +1,4 @@
+/** UI manipulation with JS */
 const imgInfo = document.querySelector('.ImgRef');
 const signInDetails = document.querySelector('.signInDetails');
 const body = document.querySelector('body');
@@ -6,8 +7,8 @@ const signupViewBtn = document.querySelector('.signupBtn');
 const signupDetails = document.querySelector('.SignupCont');
 const signInImg = document.querySelector('.signInImg');
 const signinBtn = document.querySelector('.signinBtn')
-const password = document.querySelector('.SignupPassword');
-const confPassword = document.querySelector('.passwordConfirm');
+const SignupPassword = document.querySelector('.SignupPassword')
+const confPassword = document.querySelector('.passwordConfirm')
 const signin = document.querySelector('.LoginBtn');
 const signup = document.querySelector('.getInBtn')
 
@@ -44,30 +45,93 @@ function responsiveUI () {
 }
 
 function confirmPassword() {
-  if(password.value === confPassword){
-    return;
-  }else{ alert(new Error("Your passwords doesn't match"))}
+  if(SignupPassword.value !== confPassword.value){
+    alert("Your passwords doesn't match")
+  }
 }
 
 signupViewBtn.addEventListener('click', () => {
   active = 'signup'
+  imgInfo.style.display = 'grid'
+  signInDetails.style.display = 'none';
+  signupDetails.style.display = 'grid';
   content.style.backgroundColor = 'grey';
-  signInDetails.style.display = 'none'
-  signupDetails.style.display = 'grid'
-  signInImg.style.display ='none'
+  signInImg.style.display ='none';
   responsiveUI
 })
 
 signinBtn.addEventListener('click', ()=> {
   active = 'signin';
-  signupDetails.style.display = 'none';
+  content.style.display = 'grid'
   signInDetails.style.display = 'grid';
+  signupDetails.style.display = 'none';
   content.style.backgroundColor = 'rgb(2, 2, 51)';
   responsiveUI
 })
 
-signup.addEventListener('click', confirmPassword);
+signup.addEventListener('click',
+  confirmPassword
+ );
 
-responsiveUI
+responsiveUI()
 window.addEventListener('load', responsiveUI);
 window.addEventListener('resize', responsiveUI);
+
+/** Calling of Backend codes */
+const username = document.querySelector('.email').value;
+const password = document.querySelector('.password').value;
+const fullname = document.querySelector('.fullname').value;
+const newUsername = document.querySelector('.newUsername').value;
+const newUEmail = document.querySelector('.SignupEmail').value;
+const remember = document.querySelector('.rememberMe')
+
+// Client SignUp Auth
+async function handleSignUp () {
+try{
+  const res = await fetch("/api/v1/auth/login", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({newUEmail, SignupPassword,
+      role: "CLIENT",
+      full_name: fullname
+    }),
+  });
+
+  const data = await res.json();
+  if (res.ok) {
+    if(remember) {
+      localStorage.setItem("client_email", newUEmail);
+      localStorage.setItem("client_username", newUsername);
+    }
+    alert("Signup Sccessfull 😊😎");
+    console.log(data);
+  } else{alert(data.message || "Signup Failed")}
+}catch(err) {
+  alert("Error: " + err.message);
+}
+}
+
+// Client Login Auth
+const BrealConfig = {BASE_URL: "/api/v1",
+};
+
+async function handleLogin () {
+try {
+  const res = await fetch("/api/v1/auth/login", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify({email: username, password}),
+  })
+
+  const data = await res.json();
+
+  if(res.ok) {
+    localStorage.setItem("access_tokden", data.access_token);
+    localStorage.setItem("refresh_token", data.refresh_token);
+    alert("Login sucessful 😁👌");
+    console.log(data);
+  }else{ alert(data.message || "Login failed 😣😑")}
+} catch (err) {
+  alert("Error: " + err.message);
+}
+}
